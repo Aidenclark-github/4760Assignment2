8d793eb93ae67edddb80ff47470e0686bffce104
Aidenclark-github <72275406+Aidenclark-github@users.noreply.github.com>
2021-10-06 17:22:41 -0500

    Update runsim.c

--- runsim.c
+++ runsim.c
@@ -2,26 +2,35 @@
 Main Program 
 
 */
-
 #include<stdio.h>
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<sys/types.h>
+#include<sys/file.h>
 #include<string.h>
 #include<errno.h>
 #include<stdlib.h>
+#include "header.h"
+
+extern int nlicenses;
+
 
 /* main function */
-int main ( int argc, char *argv[] )
+int main ( int argc, char *argv[] ) 
 {
-
     if ( argc != 2 ) /* argument count should be 2 for execution */
     {
-        /* print argv[0] */
         printf( "Error: %s filename", argv[0] );
         exit(1);
     }
-
+    int licenseNUMBER = atoi(argv[1]);
+    if ( initlicense() != 0)
+    {
+        printf( "Error: initlicense()");
+        exit(1);    
+    }
+    nlicenses = licenseNUMBER; 
+}
 
 
 
