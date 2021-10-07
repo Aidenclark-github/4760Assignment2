2217df53dbd1838eedf22fca9a9b24d22adea122
Aidenclark-github <72275406+Aidenclark-github@users.noreply.github.com>
2021-10-06 22:18:27 -0500

    Update license.c

--- license.c
+++ license.c
@@ -22,12 +22,43 @@ int initlicense()
     return 0;
 }
 
+/* Blocks until license is available */
 int getlicense() 
 {
+    printf("Blocking license until it is available. \n");
+    while (nlicenses <= 0);
+    removelicenses(1);
     return(0);
 }
 
+/* Increments number of licenses available  */
 int returnlicense() 
-{
+{   
+    addtolicenses(1);
     return(0);
+}
+
+/* Adds "L" licenses to number available */
+void addtolicenses (int L)
+{
+    /* Increment */
+    nlicenses += L;
+    printf("Licenses available:", nlicenses);
+    
+}
+
+void removelicenses (int L)
+{
+    /* Increment */
+    nlicenses -= L;
+    printf("Licenses available:", nlicenses);
+}
+
+void logmsg(const char * msg)
+{
+    char* filename = "data.log";
+    
+    FILE *fp;
+    fp = fopen (filename, "w");
+    fprintf(fp, "Logged Messages: ");
 }
